092dd3e231151dafa73e7966224c83b80fadb8e8
Tom Scharstein <trscharstein@gmail.com>
2020-03-15 14:55:15 -0700

    Adding lokney components, cleaning up App.js

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,24 @@ let isBrowser = !window.__adobe_cep__;
 // https://github.com/Inventsable/starlette
 import starlette from "starlette";
 
+// Utility components, see here:
+// https://github.com/Inventsable/lokney
+import { Menus, Panel } from "lokney";
+/*
+  Panel component above also includes:
+    - Starlette UI theme and color library: 
+      https://github.com/Inventsable/starlette
+    - CEP-Spy identification and app utility:
+      https://github.com/Inventsable/cep-spy
+ These are still installed into this panel and can be used when needed like so:
+ import spy from 'cep-spy'
+
+ NOTES: 
+  - Starlette is already active in your panel! There's no need to initialize it.
+  - Need CSInterface or a script? You can use the script-path attribute of Panel to launch scripts or utilities:
+    https://github.com/Inventsable/lokney/tree/master/components/Panel
+*/
+
 // Dynamic identification object that reports all panel and host information:
 // https://github.com/Inventsable/CEP-Spy
 const spy = !isBrowser
@@ -27,15 +45,13 @@ import showErrorMessage from "src/functions/function-show-error-message.js";
 export default {
   name: "App",
   components: {
-    menus: require("src/components/dev/menus.vue").default,
+    Menus,
     "render-dialog": require("src/components/panel/RenderDialog.vue").default
   },
   data: () => ({
     isMounted: false,
     isBrowser: false,
-    menus: null,
     loading: false,
-    csInterface: null
   }),
   computed: {
     ...mapGetters("settings", ["settings", "config"]),
@@ -55,29 +71,11 @@ export default {
     }
   },
   async mounted() {
-    console.clear();
-    this.isBrowser = isBrowser;
-    //
-    // Selecting the default app and user theme when in browser
-    isBrowser ? starlette.initAs("AEFT", "gradient") : starlette.init();
-    //
+
     this.loading = false;
-    console.log(
-      `${this.spy.extName} ${this.spy.extVersion} : ${
-        this.spy.isDev ? "DEV" : "BUILD"
-      }`
-    );
     //
     this.getSettings();
     // this.deleteSettings();
-    //
-    if (!isBrowser) {
-      console.log("Loading CSInterface");
-      this.csInterface = new CSInterface();
-      this.csInterface.addEventListener("console", this.consoler);
-      this.loadUniversalScripts();
-    }
-
     console.log("SETTINGS:", this.settings);
     this.$q.notify.setDefaults({
       position: "top",
@@ -120,94 +118,18 @@ export default {
         data
       );
     },
-    loadScript(path) {
-      // Correctly loads a script regardless of whether Animate or regular CEP app
-      if (!/FLPR/.test(spy.appName))
-        this.csInterface.evalScript(`$.evalFile('${path}')`);
-      else
-        this.csInterface.evalScript(
-          `fl.runScript(FLfile.platformPathToURI("${path}"))`
-        );
-    },
-    consoler(msg) {
-      // Catches all console.log() usage in .jsx files via CSEvent
-      console.log(`${spy.appName}: ${msg.data}`);
-    },
-    loadUniversalScripts() {
-      // Preloads any script located inside ./src/host/universal
-      let utilFolder = window.cep.fs.readdir(
-        `${spy.path.root}/src/host/universal/`
-      );
-      if (!utilFolder.err) {
-        let valids = utilFolder.data.filter(file => {
-          return /\.(jsx|jsfl)$/.test(file);
-        });
-        valids.forEach(file => {
-          this.loadScript(`${spy.path.root}/src/host/universal/${file}`);
-        });
-      }
-      // Preloads any script located in ./src/host/[appName]/
-      let hostFolder = window.cep.fs.readdir(
-        `${spy.path.root}/src/host/${spy.appName}/`
-      );
-      if (!hostFolder.err) {
-        let valids = hostFolder.data.filter(file => {
-          return /\.(jsx|jsfl)$/.test(file);
-        });
-        valids.forEach(file => {
-          this.loadScript(`${spy.path.root}/src/host/${spy.appName}/${file}`);
-        });
-      } else {
-        console.log(
-          `${spy.path.root}/src/host/${spy.appName} has no valid files or does not exist`
-        );
-      }
-    }
   }
 };
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
 :root {
-  --quad: cubic-bezier(0.48, 0.04, 0.52, 0.96);
-  --quart: cubic-bezier(0.76, 0, 0.24, 1);
-  --quint: cubic-bezier(0.84, 0, 0.16, 1);
   --color-shaded: rgba(0, 0, 0, 0.125);
   --toolbar-height: 48px;
   --bottombar-height: 30px;
   --min-width: 400px;
   --app-scrollbar-width: 16px;
-
-  background-color: var(--color-bg);
-  color: var(--color-default);
-  font-family: "Open Sans", sans-serif;
   min-width: var(--min-width);
-}
-
-body::-webkit-scrollbar {
-  width: var(--app-scrollbar-width);
-}
-#app::-webkit-scrollbar {
-  display: block;
-  width: var(--app-scrollbar-width);
-}
-::-webkit-scrollbar {
-  background-color: var(--color-scrollbar);
-  width: var(--app-scrollbar-width);
-}
-::-webkit-scrollbar-thumb {
-  background: var(--color-scrollbar-thumb);
-  border-radius: 20px;
-}
-::-webkit-scrollbar-thumb:hover {
-  background: var(--color-scrollbar-thumb-hover);
-}
-::-webkit-scrollbar-resizer {
-  display: none;
-}
-::-webkit-scrollbar-button {
-  height: 0px;
 }
 
 .q-card.q-dialog-plugin {
