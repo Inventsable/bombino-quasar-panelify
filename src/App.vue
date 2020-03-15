ac51da75d6823b578cc65e5e2970ea2756b159cd
Tom Scharstein <trscharstein@gmail.com>
2020-03-15 14:57:29 -0700

    Adding Panel component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="q-app">
-    <!-- <render-dialog /> -->
-    <Menus refresh debug />
-    <router-view style />
+    <Panel>
+      <!-- <render-dialog /> -->
+      <Menus refresh debug />
+      <router-view style />
+    </Panel>
   </div>
 </template>
 
